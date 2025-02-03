josephdrichard/linuxptp-daemon

Update import paths to use k8snetworkplumbingwg

addons/intel/e810.go
@@ -12,9 +12,9 @@ import (
 	"strings"
 
 	"github.com/golang/glog"
-	"github.com/openshift/linuxptp-daemon/pkg/dpll"
-	"github.com/openshift/linuxptp-daemon/pkg/plugin"
-	ptpv1 "github.com/openshift/ptp-operator/api/v1"
+	"github.com/josephdrichard/linuxptp-daemon/pkg/dpll"
+	"github.com/josephdrichard/linuxptp-daemon/pkg/plugin"
+	ptpv1 "github.com/josephdrichard/ptp-operator/api/v1"
 )
 
 type E810Opts struct {

addons/intel/phaseAdjust.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 
 	"github.com/golang/glog"
-	dpll "github.com/openshift/linuxptp-daemon/pkg/dpll-netlink"
-	ptpv1 "github.com/openshift/ptp-operator/api/v1"
+	dpll "github.com/josephdrichard/linuxptp-daemon/pkg/dpll-netlink"
+	ptpv1 "github.com/josephdrichard/ptp-operator/api/v1"
 	"sigs.k8s.io/yaml"
 )
 

edcdavid-org/ptp-operator-upstream-monorepo

pkg/linuxptp-daemon/addons/intel/e810.go
@@ -12,9 +12,9 @@ import (
 	"strings"
 
 	"github.com/golang/glog"
-	"github.com/openshift/linuxptp-daemon/pkg/dpll"
-	"github.com/openshift/linuxptp-daemon/pkg/plugin"
-	ptpv1 "github.com/openshift/ptp-operator/api/v1"
+	"github.com/josephdrichard/linuxptp-daemon/pkg/dpll"
+	"github.com/josephdrichard/linuxptp-daemon/pkg/plugin"
+	ptpv1 "github.com/k8snetworkplumbingwg/ptp-operator/api/v1"
 )
 
 type E810Opts struct {

pkg/linuxptp-daemon/addons/intel/phaseAdjust.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 
 	"github.com/golang/glog"
-	dpll "github.com/openshift/linuxptp-daemon/pkg/dpll-netlink"
-	ptpv1 "github.com/openshift/ptp-operator/api/v1"
+	dpll "github.com/josephdrichard/linuxptp-daemon/pkg/dpll-netlink"
+	ptpv1 "github.com/k8snetworkplumbingwg/ptp-operator/api/v1"
 	"sigs.k8s.io/yaml"
 )
 